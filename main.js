bd73696e716e2da0f7579c9143080a2919473f23
Elad Cohen <eladc.android@gmail.com>
2018-05-08 14:52:15 +0300

    commit branch

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
   }
-  console.log('The Generated Function:', tick);
   
-  setInterval(tick, 1000);
+function App() {
+    return (
+        <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        </div>
+    );
+}
+  
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
