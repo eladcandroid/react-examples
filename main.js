b07d70ad0aae7f4846ab620b637ce599e7392209
Elad Cohen <eladc.android@gmail.com>
2018-05-08 14:48:46 +0300

    commit branch

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+  }
+  console.log('The Generated Function:', tick);
+  
+  setInterval(tick, 1000);
